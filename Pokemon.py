0648b38984a2977bf241c9b959577360de539cb3
Daniel <111242712+DanielVargaAI@users.noreply.github.com>
2025-11-13 16:03:30 +0100

    added pokemon and settings

---
+++ Pokemon.py
@@ -0,0 +1,11 @@
+class Pokemon:
+    def __init__(self):
+        self.stats = {
+            "Atk": 0,
+            "SpA": 0,
+            "Def": 0,
+            "SpD": 0,
+            "Spe": 0,
+            "HP": 0,
+        }
+        self.dmg_types = []
